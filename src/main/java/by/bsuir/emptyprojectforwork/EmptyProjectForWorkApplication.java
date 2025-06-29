424d6bc0bf550c32b38cff385c66eb29608f307e
Pavel <pavelkarliuk1@gmail.com>
2025-06-30 01:54:05 +0300

    first commit

---
+++ src/main/java/by/bsuir/emptyprojectforwork/EmptyProjectForWorkApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.emptyprojectforwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyProjectForWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyProjectForWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/emptyprojectforwork/EmptyProjectForWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.emptyprojectforwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyProjectForWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
